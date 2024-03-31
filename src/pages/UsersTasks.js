6e82556696d80cbc18056966855ecf8675ee169f
amirros66 <143261344+amirros66@users.noreply.github.com>
2024-03-31 20:27:01 +0200

    Remove comments.

--- src/pages/UsersTasks.js
+++ src/pages/UsersTasks.js
@@ -1,40 +1,14 @@
-import React, { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { fetchUserTasks } from "../store/tasks/thunks";
-import {
-  selectTasks,
-  selectLoading,
-  selectError,
-} from "../store/tasks/selectors";
-import { useAuth } from "../hooks/useAuth";
+import React from "react";
 import ToDo from "../components/ToDo";
 import InProgress from "../components/InProgress";
 import Completed from "../components/Completed";
 import "../styling/taskPage.css";
 
 export default function UsersTasks() {
-  // const dispatch = useDispatch();
-  // const tasks = useSelector(selectTasks);
-  // const loading = useSelector(selectLoading);
-  // const error = useSelector(selectError);
-  // const { token } = useAuth();
-
-  // useEffect(() => {
-  //   if (token) {
-  //     dispatch(fetchUserTasks(token));
-  //   }
-  // }, [dispatch, token]);
-
-  // if (loading) return <div>Loading...</div>;
-  // if (error) return <div>Error: {error}</div>;
-
   return (
     <div>
       <div className="current-tasks">
         <h1>Tasks</h1>
-        {/* {tasks.map((task) => (
-          <div key={task.id}>{task.title}</div>
-        ))} */}
       </div>
       <div className="container">
         <ToDo />
